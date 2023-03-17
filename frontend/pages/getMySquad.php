2178a5182131acf29eb35a6fc8ce8c2dc8e46d2d
Simone-Giuriato <72790964+Simone-Giuriato@users.noreply.github.com>
2023-03-17 16:02:05 +0100

    .

--- frontend/pages/getMySquad.php
+++ frontend/pages/getMySquad.php
@@ -65,7 +65,7 @@ if (empty($_SESSION['user_id'])) {
         </div>
     </div>
 
-
+   
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
